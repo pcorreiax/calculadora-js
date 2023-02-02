d45e8d0c09f009f8d7512af0f20c0ad8cee6e1d9
Pedro Correia <p.correia@hotmail.com>
2023-02-02 17:03:05 -0300

    Implementa funções dos operadores

--- calculadora.js
+++ calculadora.js
@@ -1,22 +1,17 @@
 const teclas = document.querySelectorAll('.teclas')
-let visor = document.querySelector('.visor')
+const visor = document.querySelector('.visor')
+const preview = document.querySelector('.preview')
 
-const valorInicial = visor.innerText = 0 
+const valorInicial = visor.innerText = 0;
 for (const tecla of teclas) {
         tecla.addEventListener('click', e => {
             switch (e.target.dataset.char) {
-                case "somar":
-                    somar()
-                    break;
-                case "diminuir":
-                    diminuir()
-                    break;
-                case "dividir":
-                    dividir()
-                    break;
-                case "multiplicar":
-                    multiplicar()
-                    break;
+                case '+':
+                case '-':
+                case '/':
+                case '*':
+                    preencheOperadores(e)
+                    break;        
                 case "backspace":
                     backspace()
                     break;
@@ -33,18 +28,46 @@ for (const tecla of teclas) {
         })
 }
 
+const limiteVisor = 16
+
 function preencheVisor(e){
-    const limiteVisor = 10
+
     if (visor.innerText.length < limiteVisor){
         visor.innerText === '0' ? visor.innerText = e.target.dataset.char : visor.innerText += e.target.dataset.char
     }
+
+    if(visor.innerText.length > limiteVisor -3){
+        visor.style.fontSize = "35px"
+    }
+}
+
+function preencheOperadores(e) {
+    if(preview.innerText.slice(-1) === '+' || preview.innerText.slice(-1) === '-' || preview.innerText.slice(-1) === '/' || preview.innerText.slice(-1) === '*'){
+        preview.innerText = preview.innerText.slice(0, -1)
+        preview.innerText += e.target.dataset.char
+    }else{
+        preview.innerText = `${visor.innerText}  ${e.target.dataset.char}`
+        visor.innerText = 0
+    }
+    
 }
 
 function limpar(){
     visor.innerText = 0
+    visor.style.fontSize = "55px"
+    preview.innerText = ''
 }
 
 function backspace(){
      const valorCorrigido = visor.innerText.slice(0, -1)
-     valorCorrigido > 0 ?  visor.innerText = valorCorrigido : visor.innerText = 0
+     valorCorrigido > '0' ?  visor.innerText = valorCorrigido : visor.innerText = 0
+     
+    if(visor.innerText.length < limiteVisor -3){
+        visor.style.fontSize = "55px"
+    }
+}
+function enter(){
+    preview.innerText += ` ${visor.innerText}`
+    visor.innerHTML =  eval(preview.innerText)
+    preview.innerText += ' = '
 }
